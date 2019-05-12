c0a3ce03ef7932dfe1bf6c0484c9c6dac57f6493
Devin Greene <devin@greene.cz>
2019-05-11 21:44:18 -0400

    Added comments

--- symmetries4.c
+++ symmetries4.c
@@ -9,14 +9,37 @@
     graph &= 0x000000ff
 
 
-/* We encode four cube graphs with unsigned 32-bit integers.  This function
-   performs symmetry operations on the four-cube at the encoding level */
-
-/* TODO: Explain these functions! */
+/* We encode a directed graph on the 4-cube by creating an ``edge
+ * bitmask''.  Each directed edge on the 4-cube is mapped to a bit
+ * position of a 32 bit string.  The positions are indexed by symbols
+ * of the form b1.b2.*.b3, consisting of three constant bits and the
+ * star corresponding to the bit in the genotype which changes if we
+ * traverse the edge.  We refer to the constant bits b1.b2.b3 as the
+ * ``background'' bits, and view them as the number they represent in
+ * base 2.  As there are eight possible values for the background
+ * bits, and there are four positions for the star, we have 32 bits
+ * whose value indicates the direction of the directed edge.  We order
+ * the bitmask indices by moving the star from right to left, and for
+ * each star position counting from 0 to 7 and matching this number to
+ * the background bits base 2 representation.  Schematically, we have
+ *
+ * *111, *110, *101, *100, ... , 1*11, 1*10, 1*01, ... , 11*1, 11*0,
+ *       10*1, ..., 111*, 110*, 101*, 011*, 010*, 001*, 000*
+ *
+ * Any group action on the 4-cube can be decomposed into the product
+ * of a permutation on the coordinates which determine the vertices of
+ * the 4-cube and an xor-ing operation on said coordinates.  A
+ * permutation can in turn be expressed as the product of parity
+ * preserving second order group action, a single three cycle, and a
+ * single transposition.  The functions below implement these
+ * group actions via their effects on the edge bitmask representing a
+ * directed graph on the cube. */
 
 static inline ui effof01(ui n,ui shift){
-    /* Effect on the encoding of swapping the 0
-       and 1 bit at the genotype level */
+    /* 
+     * On background: xyz -> xzy
+     * Effect on edge mask: 76543210 -> 7563120 
+     */
     n >>= shift;
     ui a,b;
     a=n&0x44;
@@ -27,8 +50,10 @@ static inline ui effof01(ui n,ui shift){
 }
 
 static inline ui effof12(ui n, ui shift)
-/* This is equivalent to conjugating 
-   effof01 with effof201 */
+    /* 
+     * On background: xyz -> yxz
+     * Effect on edge mask: 76543210 -> 76325410
+     */
 {
     n >>= shift;
     ui a,b;
@@ -41,6 +66,10 @@ static inline ui effof12(ui n, ui shift)
 
 static inline ui effof201(ui n,ui shift,ui inv)
 {
+    /*
+     * !inv on background: xyz -> zxy
+     * Effect on edge mask: 76543210 -> 73625140
+     */ 
     n >>= shift;
     ui a,b,c,d,e,f;
     a = 0x40&n;
@@ -62,7 +91,7 @@ ui symmetry(ui graph,ui pflip,ui cycle,ui tau,ui xorop)
     ui a,b,c;
     SPLIT_UP;
     switch(pflip){
-        case 1: // abcd -> badc
+        case 1: // 3210 -> 2301
             a = effof01(a,24);
             b = effof01(b,16);
             c = effof12(c,8);
@@ -70,7 +99,7 @@ ui symmetry(ui graph,ui pflip,ui cycle,ui tau,ui xorop)
             graph <<= 8;
             graph += (a>>8) + (b<<8) + (c>>8);
             break;
-        case 2: // abcd -> cdab
+        case 2: // 3210 -> 1032
             a = effof201(a,24,1);
             b = effof201(b,16,1);
             c = effof201(c,8,0);
@@ -78,7 +107,7 @@ ui symmetry(ui graph,ui pflip,ui cycle,ui tau,ui xorop)
             graph <<= 16; 
             graph += (a>>16) + (b>>16) + (c<<16);
             break;
-        case 3: // abcd -> dcba
+        case 3: // 3210 -> 0123
             a = effof01(effof201(a,24,0),24);
             b = effof01(effof201(b,16,0),16);
             c = effof01(effof201(c,8,0),8);
@@ -93,14 +122,14 @@ ui symmetry(ui graph,ui pflip,ui cycle,ui tau,ui xorop)
 
     SPLIT_UP;
     switch(cycle){
-        case 1: // abcd -> adbc
+        case 1: // 3210 -> 3021
             a=effof201(a,24,0);
             b=effof01(b,16);
             c=effof01(c,8);
             graph <<= 16;
             graph += a + (b>>8) + (c>>8);
             break;
-        case 2: // abcd -> acdb
+        case 2: // 3210 -> 3102
             a=effof201(a,24,1);
             c=effof01(c,8);
             graph=effof01(graph,0);
@@ -112,7 +141,7 @@ ui symmetry(ui graph,ui pflip,ui cycle,ui tau,ui xorop)
     }   
 
     SPLIT_UP;
-    if(tau){ // ABCD -> ABDC
+    if(tau){ // 3210 -> 3201
         a = effof01(a,24);
         b = effof01(b,16);
         graph <<= 8;
